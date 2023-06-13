ac232e2c27ca5d6af3830cd692a44e1783fa5702
JarLaX99 <vanek10699@gmail.com>
2023-06-13 21:15:29 +0300

    первая задача

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 99 && numberA < 1000)
+{
+    Console.WriteLine($"{(numberA / 10) % 10}");
+}
+else
+{
+    Console.WriteLine(" Ошибка : Введите трёхзначное число");
+}
