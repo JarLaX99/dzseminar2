cad2adc0e46f2625449c31fa5c14741a88aa58eb
JarLaX99 <vanek10699@gmail.com>
2023-06-13 22:21:14 +0300

    Третья задача

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 0 && numberA < 6)
+{
+    Console.WriteLine("Будний день");
+}
+else if (numberA > 5 && numberA < 8)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("такого дня недели не существует");
+}
