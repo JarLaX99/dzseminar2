2b30a97ac645bfb777a072df78d5b0acd226102e
JarLaX99 <vanek10699@gmail.com>
2023-06-13 22:12:13 +0300

    вторая задача

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > 99)
+{
+    while (numberA > 1000)
+    {
+        numberA = numberA / 10;
+    }
+    Console.WriteLine($"{numberA % 10}");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
